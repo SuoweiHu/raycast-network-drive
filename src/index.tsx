18bc50c5845f1d0128567608e0c957e0d2677ddb
Suowei Hu <suowei.h@gmail.com>
2024-05-30 08:43:44 +1000

    Feat: delay when mount/unmount to make better UX

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { get_pref_smb_ip, get_pref_smb_pwd, get_pref_smb_usr } from "./utils-preference";
 import { smbclient_getVolumes } from "./utils-volumes";
 
+// DELAY HELPER FUNCTION
+function delay(milliseconds:number) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
 // TOP LEVEL LIST
 function List_SMB_Volumes() {
     let [volumes, set_volumes] = useState<string[]>([])
@@ -37,6 +42,7 @@ function List_SMB_ActionPanel(props:{vol:string,vols:string[]}){
                 shortcut={{ modifiers: [], key: "enter" }}
                 onAction={async () => {
                     showToast({title:"Mounting...", style:Toast.Style.Animated});
+                    await delay(1000);
                     exec(`osascript -e 'mount volume "smb://${get_pref_smb_ip()}/${props.vol}"'`, async (err, stdout, stderr) => {
                         if (err) {showHUD("UN-MOUNTED FAILURE !")}
                         exec(`open "/Volumes/${props.vol}"`);
@@ -49,7 +55,7 @@ function List_SMB_ActionPanel(props:{vol:string,vols:string[]}){
                 shortcut={{ modifiers: ["ctrl"], key: "x" }}
                 onAction={async () => {
                     showToast({title:"Un-Mounting...", style:Toast.Style.Animated});
-                    console.log(`/usr/sbin/diskutil unmount "/Volumes/${props.vol}"`);
+                    await delay(1000);
                     exec(`/usr/sbin/diskutil unmount "/Volumes/${props.vol}"`, async (err, stdout, stderr) => {
                         if(stdout.includes("Unmount successful")){showHUD("UN-MOUNTED 🪂🌍")}
                         else{showHUD("UN-MOUNTED FAILURE !")}
@@ -61,6 +67,7 @@ function List_SMB_ActionPanel(props:{vol:string,vols:string[]}){
                 shortcut={{ modifiers: ["ctrl", "shift"], key: "x" }}
                 onAction={async () => {
                     showToast({title:"Un-Mounting All...", style:Toast.Style.Animated});
+                    await delay(1000);
                     props.vols.forEach((_vol_)=>{
                         exec(`/usr/sbin/diskutil unmount unmount "/Volumes/${_vol_}"`, async (err, stdout, stderr) => {
                             if (err) {showHUD("UN-MOUNTED ALL FAILURE !")}
